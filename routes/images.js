7646e1f2714e1a9f9fd36bad0779e3a9b31e5864
Luiz Picanço <lpicanco@gmail.com>
2015-09-27 21:59:24 -0300

    Added scale service

--- routes/images.js
+++ routes/images.js
@@ -2,83 +2,75 @@ var express = require('express');
 var router = express.Router();
 var multiparty = require('multiparty');
 var lwip = require('lwip');
-
+var request = require('request').defaults({encoding: null});
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
     res.writeHead(200, {'content-type': 'text/html'});
     res.end(
-        '<form action="/images/upload" enctype="multipart/form-data" method="post">' +
+        '<form action="/api/image/upload" enctype="multipart/form-data" method="post">' +
         '<input type="file" name="upload"><br>' +
         '<input type="submit" value="Upload">' +
         '</form>'
     );
 });
 
+router.get("/scale", function (req, res, next) {
+    var url = req.query.imageUrl;
+    var imageType = req.query.imageType;
+    var ratio = parseFloat(req.query.ratio);
+
+    request.get(url, function (err, imageReq, data) {
+        lwip.open(data, imageType, function (err, image) {
+            console.log("error", err);
+            image.batch()
+                .scale(ratio)
+                .toBuffer(imageType, function (err, buffer) {
+                    res.end(buffer)
+                });
+        });
+    });
+
+
+});
+
 router.post('/upload', function (req, res, next) {
     var form = new multiparty.Form();
 
-    var destPath;
-    form.on('field', function (name, value) {
-        console.log("name: ", name);
-        console.log("value: ", value);
-        if (name === 'path') {
-            destPath = value;
-        }
-    });
     form.on('part', function (part) {
-        console.log("Received bytes count: ", part.byteCount)
-        //console.log("Received bytes raw: ", part)
         var bufs = [];
 
+        var imageType = extractImageType(part);
+        console.log("Image Type", imageType);
+
         part.on("data", function (data) {
-            //console.log("Received bytes raw: ", data)
+            console.log("data", data);
             bufs.push(data);
         });
 
         part.on("end", function () {
-            console.log("End stream")
             var buffer = Buffer.concat(bufs);
 
-            lwip.open(buffer, "jpg", function (err, image) {
-                console.log("Errors: ", err);
+            lwip.open(buffer, imageType, function (err, image) {
                 image.batch()
-                    .scale(0.1)
-                    .toBuffer("jpg", function (err, buffer) {
+                    .scale(0.5)
+                    .toBuffer(imageType, function (err, buffer) {
                         res.end(buffer)
-                        console.log(err);
+                        //console.log(err);
                     });
             });
         });
 
         part.resume();
 
-
-        /*
-         s3Client.putObject({
-         Bucket: bucket,
-         Key: destPath,
-         ACL: 'public-read',
-         Body: part,
-         ContentLength: part.byteCount,
-         }, function(err, data) {
-         if (err) throw err;
-         console.log("done", data);
-         res.end("OK");
-         console.log("https://s3.amazonaws.com/" + bucket + '/' + destPath);
-         });*/
     });
     form.parse(req);
-    //res.end("OK");
-
-    /*
-     var file = req.files.file;
-     console.log(file.name);
-     console.log(file.type);
-     console.log(file.path);
-
-     res.send('respond with a resource');
-     */
 });
+
+function extractImageType(part) {
+    var contentType = part.headers["content-type"];
+    return contentType.substring(contentType.indexOf('/') + 1);
+}
+
 
 module.exports = router;
